119c454feb5c57c2e170cbe282da87716ac015fa
Elias Bistry <eliasoderso@riseup.net>
2024-12-08 01:57:18 +0100

    assign auth cert to kanidm group

--- apps/auth.nix
+++ apps/auth.nix
@@ -10,9 +10,8 @@ in {
       origin = "https://${fqdn}";
       bindaddress = "127.0.0.1:8001";
       ldapbindaddress = "127.0.0.1:636";
-      # db_path = "/var/lib/kanidm/kanidm.db"; -> readonly
-      tls_chain = "/var/lib/acme/${fqdn}/chain.pem";
-      tls_key = "/var/lib/acme/${fqdn}/key.pem";
+      tls_chain = "/var/lib/kanidm/chain.pem";
+      tls_key = "/var/lib/kanidm/key.pem";
       online_backup = {
         versions = 3;
         path = "/var/lib/kanidm/backups";
@@ -30,4 +29,10 @@ in {
       recommendedProxySettings = true;
     };
   };
+
+  security.acme.certs."${fqdn}".postRun = ''
+    cp key.pem /var/lib/kanidm && cp chain.pem /var/lib/kanidm
+    chown kanidm:kanidm /var/lib/kanidm/key.pem && chown kanidm:kanidm /var/lib/kanidm/chain.pem
+    chmod 400 /var/lib/kanidm/key.pem && chmod 400 /var/lib/kanidm/chain.pem
+  '';
 }
