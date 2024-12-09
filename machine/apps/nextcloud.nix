87318e40f5285862fd383f218d8ec8306a6b2cd6
Elias Bistry <eliasoderso@riseup.net>
2024-12-10 00:58:12 +0100

    add admin password

--- machine/apps/nextcloud.nix
+++ machine/apps/nextcloud.nix
@@ -1,11 +1,15 @@
-{pkgs,...}:
+{pkgs, config, ...}:
 {
+  sops.secrets."services/nextcloud/adminPassword" = {};
   services.nextcloud = {
     enable = true;
     hostName = "cloud.systemlos.org";
     package = pkgs.nextcloud30;
+
+    config.adminpassFile = config.sops.secrets."services/nextcloud/adminPassword".path;
+
     extraApps = {
-    inherit (config.services.nextcloud.package.packages.apps) news contacts calendar tasks;
-  };
+      inherit (config.services.nextcloud.package.packages.apps) news contacts calendar tasks;
+    };
   };
 }
