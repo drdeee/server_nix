6e4aa54a9d6eef9957d97480f39729954b0c0623
Elias Bistry <eliasoderso@riseup.net>
2024-12-09 05:08:53 +0100

    some changes on maddy config

--- machine/apps/mail.nix
+++ machine/apps/mail.nix
@@ -41,12 +41,12 @@ in {
       auth.ldap local_ldap {
         urls ldap://${lldap.ldap_host}:${toString lldap.ldap_port}
 
-        bind plain "cn=system,ou=people,${baseDN}" "{env:LDAP_PASSWORD}"
-        filter "(&(&(objectClass=person)(mail={username}))(memberOf=cn=mail,ou=groups,${baseDN}))"
+        bind plain "uid=system,ou=people,${baseDN}" "{env:LDAP_PASSWORD}"
+        filter "(&(&(objectClass=person)(mail={username}))(memberOf=uid=mail,ou=groups,${baseDN}))"
         base_dn "${baseDN}"
 
         starttls off
-        debug off
+        debug on
         connect_timeout 1m
       }
       ${lib.readFile ../config/maddy.conf}
