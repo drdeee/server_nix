db9931b19fcbbaba66af2fd3f7595bf4b499c646
Elias Bistry <eliasoderso@riseup.net>
2024-12-19 06:22:13 +0100

    some optimisations

--- machine/backup.nix
+++ machine/backup.nix
@@ -7,59 +7,43 @@ let
     "--keep-weekly 5"
   ];
 
-  generateBackupConfig = backupName: backupLocations: {
+  generateBackupConfig = backupName: backupLocations: let
+    serviceName = config.systemd.services."restic-backups-${backupName}".name;
+    defaultResticConf = {
+      initialize = true;
+      paths = backupLocations;
+      passwordFile = config.sops.secrets."restic/passwords/${backupName}".path;
+      rcloneConfigFile = config.sops.secrets."restic/rclone".path;
+      pruneOpts = pruneOpts;
+      timerConfig = null;
+    };
+    runAfterLocal = {
+      wantedBy = [ serviceName ];
+      after = [ serviceName ];
+    };
+  in {
     sops.secrets."restic/rclone" = {};
     sops.secrets."restic/passwords/${backupName}" = {};
 
-    services.restic.backups."${backupName}" = {
-      initialize = true;
-      paths = backupLocations;
+    services.restic.backups."${backupName}" = defaultResticConf // {
       repository = "/var/lib/restic/${backupName}";
-      passwordFile = config.sops.secrets."restic/passwords/${backupName}".path;
-      pruneOpts = [
-        "--keep-daily 3"
-        "--keep-weekly 5"
-      ];
       timerConfig.OnCalendar = "*-*-* 4:00:00";
     };
 
-    services.restic.backups."${backupName}-remote" = {
-      initialize = true;
-      paths = backupLocations;
+    services.restic.backups."${backupName}-remote" = defaultResticConf // {
       repository = "rclone:remote:/${remoteBase}/${backupName}";
-      rcloneConfigFile = config.sops.secrets."restic/rclone".path;
-      passwordFile = config.sops.secrets."restic/passwords/${backupName}".path;
-      pruneOpts = pruneOpts;
-      timerConfig = null;
     };
 
-    services.restic.backups."${backupName}-mirror" = {
-      initialize = true;
-      paths = backupLocations;
+    services.restic.backups."${backupName}-mirror" = defaultResticConf // {
       repository = "rclone:mirror:/${mirrorBase}/${backupName}";
-      rcloneConfigFile = config.sops.secrets."restic/rclone".path;
-      passwordFile = config.sops.secrets."restic/passwords/${backupName}".path;
-      pruneOpts = pruneOpts;
-      timerConfig = null;
     };
 
-    systemd.services."restic-backups-${backupName}-remote" = {
-      wantedBy = [
-        config.systemd.services."restic-backups-${backupName}".name
-      ];
-      after = [
-        config.systemd.services."restic-backups-${backupName}".name
-      ];
-    };
+    systemd.services."restic-backups-${backupName}-remote" = runAfterLocal;
 
     systemd.services."restic-backups-${backupName}-mirror" = {
-      wantedBy = [
-        config.systemd.services."restic-backups-${backupName}".name
-      ];
-      after = [
-        config.systemd.services."restic-backups-${backupName}".name
-      ];
-      serviceConfig.ExecStart = [
+      wantedBy = runAfterLocal.wantedBy;
+      after = runAfterLocal.after;
+      serviceConfig.ExecStart = lib.mkForce [
         "${pkgs.restic}/bin/restic copy --from-repo /var/lib/restic/${backupName} --from-password-file ${config.sops.secrets."restic/passwords/${backupName}".path}"
         "${pkgs.restic}/bin/restic forget --prune ${lib.concatStringsSep " " pruneOpts}"
       ];
