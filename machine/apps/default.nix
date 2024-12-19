207c45211fb3ed3209034288c17d9475ed22c1c3
Elias Bistry <eliasoderso@riseup.net>
2024-12-20 00:11:38 +0100

    undo lldap change

--- machine/apps/default.nix
+++ machine/apps/default.nix
@@ -5,7 +5,8 @@
 
     ./metrics.nix
     ./vaultwarden.nix
-    # ./nextcloud.nix
+
+    ./nextcloud.nix
     # ./keycloak.nix
   ];
 }
--- machine/apps/nextcloud.nix
+++ machine/apps/nextcloud.nix
@@ -1,4 +1,4 @@
-{pkgs, config, ...}:
+{ pkgs, config, ... }:
 {
 
   sops.secrets."services/nextcloud/adminPassword" = {
