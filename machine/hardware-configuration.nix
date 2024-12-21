f6b05e536c1c7a2a259f61a48bf2217e57921484
Elias Bistry <eliasoderso@riseup.net>
2024-12-21 01:11:42 +0100

    change hardware config

--- machine/hardware-configuration.nix
+++ machine/hardware-configuration.nix
@@ -1,8 +1,8 @@
 { modulesPath, ... }:
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = "/dev/vda";
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
   boot.initrd.kernelModules = [ "nvme" ];
-   fileSystems."/" = { device = "/dev/sda3"; fsType = "ext4"; };
+  fileSystems."/" = { device = "/dev/vda1"; fsType = "ext4"; };
 }
